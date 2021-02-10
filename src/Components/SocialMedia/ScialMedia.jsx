b8f4dfdad60487c29c918d5678a5c163dfef56bd
Pawel Bozejewski <bozejewski@gmail.com>
2021-02-10 15:32:54 +0100

    parallax

--- src/Components/SocialMedia/ScialMedia.jsx
+++ src/Components/SocialMedia/ScialMedia.jsx
@@ -1,78 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Title from '../../common/title';
+import { Parallax } from 'react-parallax';
+import img1 from '../../assets/offert1.jpg';
+import img2 from '../../assets/offert2.jpg';
+import img3 from '../../assets/offert3.jpg';
+
+const inlineStyle = {
+  background: '#fff',
+  position: 'absolute',
+  top: '46.5%',
+  left: '46.5%',
+  padding: '1.5rem',
+};
+
+const bgStyle1 = { backgroundPosition: 'bottom' };
 
 const SocialMedia = () => {
-  // var ticking = false;
-  // var isFirefox = /Firefox/i.test(navigator.userAgent);
-  // var isIe =
-  //   /MSIE/i.test(navigator.userAgent) ||
-  //   /Trident.*rv\:11\./i.test(navigator.userAgent);
-  // var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
-  // var slideDurationSetting = 600; //Amount of time for which slide is "locked"
-  // var currentSlideNumber = 0;
-  // var totalSlideNumber = $('.background').length;
-
-  // ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
-  // function parallaxScroll(evt) {
-  //   if (isFirefox) {
-  //     //Set delta for Firefox
-  //     delta = evt.detail * -120;
-  //   } else if (isIe) {
-  //     //Set delta for IE
-  //     delta = -evt.deltaY;
-  //   } else {
-  //     //Set delta for all other browsers
-  //     delta = evt.wheelDelta;
-  //   }
-
-  //   if (ticking != true) {
-  //     if (delta <= -scrollSensitivitySetting) {
-  //       //Down scroll
-  //       ticking = true;
-  //       if (currentSlideNumber !== totalSlideNumber - 1) {
-  //         currentSlideNumber++;
-  //         nextItem();
-  //       }
-  //       slideDurationTimeout(slideDurationSetting);
-  //     }
-  //     if (delta >= scrollSensitivitySetting) {
-  //       //Up scroll
-  //       ticking = true;
-  //       if (currentSlideNumber !== 0) {
-  //         currentSlideNumber--;
-  //       }
-  //       previousItem();
-  //       slideDurationTimeout(slideDurationSetting);
-  //     }
-  //   }
-  // }
-
-  // // ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
-  // function slideDurationTimeout(slideDuration) {
-  //   setTimeout(function () {
-  //     ticking = false;
-  //   }, slideDuration);
-  // }
-
-  // // ------------- ADD EVENT LISTENER ------------- //
-  // var mousewheelEvent = isFirefox ? 'DOMMouseScroll' : 'wheel';
-  // window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
-
-  // // ------------- SLIDE MOTION ------------- //
-  // function nextItem() {
-  //   var $previousSlide = $('.background').eq(currentSlideNumber - 1);
-  //   $previousSlide.removeClass('up-scroll').addClass('down-scroll');
-  // }
-
-  // function previousItem() {
-  //   var $currentSlide = $('.background').eq(currentSlideNumber);
-  //   $currentSlide.removeClass('down-scroll').addClass('up-scroll');
-  // }
-
   return (
     <>
-      <Title titleText='social media' />
-      <div>social media</div>
+      <Title titleText='social media' style={{ textAlign: 'center' }} />
+      <div className='container'>
+        <Parallax bgImage={img1} style={bgStyle1} strength={250}>
+          <div style={{ height: 600 }}>
+            <div style={inlineStyle}> Oferta 1 </div>
+          </div>
+        </Parallax>
+        <h1 style={{ fontSize: '3rem' }}> ||| </h1>
+        <Parallax bgImage={img2} blur={{ min: -5, max: 6 }}>
+          <div style={{ height: 600 }}>
+            <div style={inlineStyle}> Oferta 1 </div>
+          </div>
+        </Parallax>
+        <div style={{ height: '100vh' }}></div>
+      </div>
     </>
   );
 };
